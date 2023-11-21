725ae8435768a7a2ade3acc4154b824102099232
ALexus <vorgen.morgen@yandex.ru>
2023-11-21 23:25:29 +0200

    preregistration-form.jsx

--- src/components/home/preregistration-form.jsx
+++ src/components/home/preregistration-form.jsx
@@ -7,6 +7,16 @@ export function PreregistrationForm() {
                 <span className="title">Попередній запис</span>
                 <div className="side-line"></div>
             </div>
+            <div className="under-title">
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                З вами зв'яжуться, дадуть відповідь на ваші запитання,
+                а також запишуть на прийом
+            </div>
+            <form action="" className="preregistration-form">
+                <input type="text" placeholder="Ваше ім'я"/>
+                <input type="text" placeholder="Контактний телефон"/>
+                <button>Передзвонити мені</button>
+            </form>
         </div>
     )
 }
