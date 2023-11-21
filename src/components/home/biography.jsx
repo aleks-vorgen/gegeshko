857ff427832784b353fad78f917bc56ce25f464d
ALexus <vorgen.morgen@yandex.ru>
2023-11-21 23:24:59 +0200

    biography.jsx

---
+++ src/components/home/biography.jsx
@@ -0,0 +1,18 @@
+export function Biography() {
+    return (
+        <div className="home-bio">
+            <div className="bio">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div className="image">
+                <img src="/images/home-logo.svg" alt=""/>
+            </div>
+        </div>
+    )
+}
