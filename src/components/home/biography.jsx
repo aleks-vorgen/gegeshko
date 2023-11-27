391a9dacd36ed6f255ff0d1ba73757664784ce59
ALexus <vorgen.morgen@yandex.ru>
2023-11-27 22:06:34 +0200

    Bio text imported

--- src/components/home/biography.jsx
+++ src/components/home/biography.jsx
@@ -2,13 +2,15 @@ export function Biography() {
     return (
         <div className="home-bio">
             <div className="bio">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                Вітаю! Я Гегешко Віталій Васильович лікар психіатр-нарколог вищої категорії,
+                кандидат медичних наук. Мій досвід роботи в практичній медицині 18 років. Весь цей
+                час працюю у відділенні детоксикації та інтенсивної терапії КНП СОР «ОКМЦСНЗ».
+                Також займаюсь науковою та викладацькою діяльністю на кафедрі нейрохірургії та
+                неврології медичного інституту Сумського державного університету. Маю приватну
+                медичну практику.<br/>
+                Метою своєї роботи вважаю допомогти пацієнтам навчитись досягати поставленої
+                мети - бути мотивованими на здоровий спосіб життя та мати психічну і емоційну
+                стабільність в умовах сьогодення.
             </div>
             <div className="image">
                 <img src="/images/profile-horizontal.png" alt=""/>
